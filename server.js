d60c94a7cecc4626467985ea54b9551f315990cb
MUNIF <munif.shellbeehaken@gmail.com>
2023-02-14 14:39:20 +0600

    add code for production build.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 //3.declare bodyparser for take request and take data from body
 const bodyParser = require('body-parser');
+//for production
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -24,6 +26,16 @@ mongoose
 
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
