bd0d8d89dca48885542dc8f4fe7fbf3ca4fc96f1
MUNIF <munif.shellbeehaken@gmail.com>
2023-02-15 13:20:36 +0600

    complete backend for authentication feature.

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://sbh:sbh2242@cluster0.vg5zpgz.mongodb.net/?retryWrites=true&w=majority',
-};
--- server.js
+++ server.js
@@ -3,37 +3,43 @@ const express = require('express');
 //2.declare mongoose ORM for interect with mongodb database
 const mongoose = require('mongoose');
 //3.declare bodyparser for take request and take data from body
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 //for production
 const path = require('path');
+const config = require('config');
 
-const items = require('./routes/api/items');
+//const items = require('./routes/api/items');
 
 //4.initialize express
 const app = express();
 
 //BodyParser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 //Connect to Mongo
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    //userCreateIndex: true
+  })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
 //Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('./client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
